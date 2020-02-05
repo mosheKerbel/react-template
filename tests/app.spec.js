f5cad37e449248fb1cb08e12f5e655d9b496040b
moshek <moshek@wix.com>
2020-02-05 12:02:38 +0200

    adding jest

---
+++ tests/app.spec.js
@@ -0,0 +1,9 @@
+// Using react-test-renderer
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../src/App';
+describe('Counter', () => {
+  test('snapshot renders', () => {
+    const component = renderer.create(<App counter={1} />);
+  });
+});
